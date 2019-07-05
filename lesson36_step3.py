daeca4724b7962c77651a0679d2d5df61f82a7be
Фролов Никита <NFrolov@medlinesoft.ru>
2019-07-05 16:00:44 +0300

    working version of tests

--- lesson36_step3.py
+++ lesson36_step3.py
@@ -6,6 +6,7 @@ import time
 import math
 import pytest
 
+
 @pytest.fixture()
 def browser():
     print("\nstart browser for test..")
@@ -15,8 +16,11 @@ def browser():
     browser.quit()
 
 
-@pytest.mark.parametrize('get_link', ["https://stepik.org/lesson/236895/step/1", "https://stepik.org/lesson/236896/step/1", "https://stepik.org/lesson/236897/step/1", "https://stepik.org/lesson/236898/step/1",
-                                      "https://stepik.org/lesson/236899/step/1", "https://stepik.org/lesson/236903/step/1","https://stepik.org/lesson/236904/step/1", "https://stepik.org/lesson/236905/step/1"])
+@pytest.mark.parametrize('get_link',
+                         ["https://stepik.org/lesson/236895/step/1", "https://stepik.org/lesson/236896/step/1",
+                          "https://stepik.org/lesson/236897/step/1", "https://stepik.org/lesson/236898/step/1",
+                          "https://stepik.org/lesson/236899/step/1", "https://stepik.org/lesson/236903/step/1",
+                          "https://stepik.org/lesson/236904/step/1", "https://stepik.org/lesson/236905/step/1"])
 class TestAnswer(object):
     def test_send_answer_for_pages(self, browser, get_link):
         link = get_link
@@ -26,9 +30,10 @@ class TestAnswer(object):
         answer = math.log(int(time.time()))
         input.send_keys(str(answer))
 
-        #WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.TAG_NAME, "button")))
+        # WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.TAG_NAME, "button")))
         button = browser.find_element_by_tag_name("button")
         button.click()
-        message = WebDriverWait(browser, 3).until(EC.visibility_of((By.CLASS_NAME, "smart-hints__hint")))
-        assert message == "Correct!"
-        
+
+        message = browser.find_element_by_class_name("smart-hints__hint").text
+        print("\n" + message)
+        WebDriverWait(browser, 3).until(EC.text_to_be_present_in_element((By.CLASS_NAME, "smart-hints__hint"), "Correct!"))
